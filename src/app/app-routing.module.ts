5bde258a46fadbb533debb4a4c55b1832c8f36f3
lucie <weber_lucie@web.de>
2022-03-01 10:44:30 +0100

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainpageComponent} from "./views/mainpage/mainpage.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'mainpage', component: MainpageComponent},
+  {path: '', redirectTo: '/mainpage', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
