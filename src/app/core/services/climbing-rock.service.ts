cc5cf0aa3464082bfd65525170930cd112f8c55e
lucie <weber_lucie@web.de>
2022-04-01 15:44:00 +0200

    add climbing rock service

---
+++ src/app/core/services/climbing-rock.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {ClimbingRock} from "../model/entities/climbing-rock.model";
+import {BehaviorSubject} from "rxjs";
+import {isNotNullOrUndefined} from "codelyzer/util/isNotNullOrUndefined";
+import {filter} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClimbingRockService {
+
+  constructor() { }
+
+  private readonly climbingRockListSubject = new BehaviorSubject<ClimbingRock[]>([]);
+  public readonly climbingRockList$ = this.climbingRockListSubject.asObservable().pipe(
+    filter(isNotNullOrUndefined),
+  );
+
+  get climbingRocks(): ClimbingRock[] {
+    return this.climbingRockListSubject.value;
+  }
+
+  addClimbingRock(climbingRock: ClimbingRock): void{
+    this.climbingRockListSubject.next([
+      ...this.climbingRocks,
+      climbingRock
+    ]);
+  }
+}
