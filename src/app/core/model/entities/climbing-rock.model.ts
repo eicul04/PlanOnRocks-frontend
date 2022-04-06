2dbf4b94023ea2756755887fafb8afd6bcfe577a
lucie <weber_lucie@web.de>
2022-04-06 20:15:45 +0200

    refactoring: delete location vo

--- src/app/core/model/entities/climbing-rock.model.ts
+++ src/app/core/model/entities/climbing-rock.model.ts
@@ -1,15 +1,14 @@
 import {Difficulty} from "../valueObjects/difficulty.enum";
 import {Bolting} from "../valueObjects/bolting.enum";
-import {Location} from "../valueObjects/location.model"
 
 export class ClimbingRock {
 
-  location: Location;
+  location: string;
   name: string;
   difficulty: Difficulty;
   bolting: Bolting;
 
-  constructor(location: Location, name: string, difficulty: Difficulty, bolting: Bolting) {
+  constructor(location: string, name: string, difficulty: Difficulty, bolting: Bolting) {
     this.location = location;
     this.name = name;
     this.difficulty = difficulty;
